0dd7dec5ecf0eafdb79109c97afa8abcbb74b034
sourcepirate <plasmashadowx@gmail.com>
2018-04-04 08:03:01 +0800

    Enabling stero

--- record.js
+++ record.js
@@ -10,18 +10,19 @@
         minPitch = 0.8,
         maxPitch = 1.5,
         overLapRatio = 0.5,
+        totalChannels = 2,
         btn = document.getElementById('letstart'),
         p = document.createElement('p');
-    
-    document.body.appendChild(p);
 
+    document
+        .body
+        .appendChild(p);
 
-    function getGesture(){
+    function getGesture() {
         return new Promise((res, rej) => {
             btn.onclick = res;
         });
     }
-
 
     // create a hann window to transform
     function hann(length) {
@@ -65,91 +66,94 @@
             var inputBuffer = audioProcessingEvent.inputBuffer;
             var outputBuffer = audioProcessingEvent.outputBuffer;
 
-            var inputData = inputBuffer.getChannelData(0);
-            var outputData = outputBuffer.getChannelData(0);
+            for (var ch = 0; ch < totalChannels; ch++) {
 
-            for (i = 0; i < inputData.length; i++) {
+                var inputData = inputBuffer.getChannelData(ch);
+                var outputData = outputBuffer.getChannelData(ch);
 
-                // Apply the window to the input buffer
-                inputData[i] *= this.grainWindow[i];
+                for (i = 0; i < inputData.length; i++) {
 
-                // Shift half of the buffer
-                this.buffer[i] = this.buffer[i + grainSize];
+                    // Apply the window to the input buffer
+                    inputData[i] *= this.grainWindow[i];
 
-                // Empty the buffer tail
-                this.buffer[i + grainSize] = 0.0;
-            }
+                    // Shift half of the buffer
+                    this.buffer[i] = this.buffer[i + grainSize];
 
-            var grainData = new Float32Array(grainSize * 2);
-            var grainDataLow = new Float32Array(grainSize * 2);
-            var grainDataHigh = new Float32Array(grainSize * 2);
+                    // Empty the buffer tail
+                    this.buffer[i + grainSize] = 0.0;
+                }
 
-            for (var i = 0, j = 0.0; i < grainSize; i++, j += minPitch) {
-                var index = Math.floor(j) % grainSize;
-                var a = inputData[index];
-                var b = inputData[(index + 1) % grainSize];
-                grainDataLow[i] += linearInterPol(a, b, j % 1.5) * this.grainWindow[i];
-            }
+                var grainData = new Float32Array(grainSize * 2);
+                var grainDataLow = new Float32Array(grainSize * 2);
+                var grainDataHigh = new Float32Array(grainSize * 2);
 
-            for (var i = 0, j = 0.0; i < grainSize; i++, j += maxPitch) {
-                var index = Math.floor(j) % grainSize;
-                var a = inputData[index];
-                var b = inputData[(index + 1) % grainSize];
-                grainDataHigh[i] += linearInterPol(a, b, j % 1.5) * this.grainWindow[i];
-            }
+                for (var i = 0, j = 0.0; i < grainSize; i++, j += minPitch) {
+                    var index = Math.floor(j) % grainSize;
+                    var a = inputData[index];
+                    var b = inputData[(index + 1) % grainSize];
+                    grainDataLow[i] += linearInterPol(a, b, j % 1.5) * this.grainWindow[i];
+                }
 
-            for (i = 0; i < grainSize; i += Math.round(grainSize * (1 - overLapRatio))) {
-                for (j = 0; j <= grainSize; j++) {
-                    this.buffer[i + j] += grainDataLow[j] / 3.0 + (3 * grainDataHigh[j]);
+                for (var i = 0, j = 0.0; i < grainSize; i++, j += maxPitch) {
+                    var index = Math.floor(j) % grainSize;
+                    var a = inputData[index];
+                    var b = inputData[(index + 1) % grainSize];
+                    grainDataHigh[i] += linearInterPol(a, b, j % 1.5) * this.grainWindow[i];
+                }
+
+                for (i = 0; i < grainSize; i += Math.round(grainSize * (1 - overLapRatio))) {
+                    for (j = 0; j <= grainSize; j++) {
+                        this.buffer[i + j] += grainDataLow[j] / 3.0 + (3 * grainDataHigh[j]);
+                    }
+                }
+
+                // Output the first half of the buffer
+                for (i = 0; i < grainSize; i++) {
+                    outputData[i] = this.buffer[i];
                 }
             }
-
-            // Output the first half of the buffer
-            for (i = 0; i < grainSize; i++) {
-                outputData[i] = this.buffer[i];
-            }
         }
-
         source.buffer = buf;
         source.connect(filter);
         filter.connect(audioCtx.destination);
         source.start();
     }
 
-
-
     if ('MediaRecorder' in wnd) {
         // start recording
-        getGesture().then(getStream).then((stream) => {
+        getGesture()
+            .then(getStream)
+            .then((stream) => {
 
-            // initialize the recorder
+                // initialize the recorder
 
-            p.innerHTML = "Recording what you are speaking....";
-            recorder = new MediaRecorder(stream);
-            recorder.start();
-            recorder.ondataavailable = (e) => {
-                chunks.push(e.data)
-            }
-            // export the stored data into audio clip
-            recorder.onstop = (e) => {
-                var audioBlob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
-                var audioContext = new AudioContext();
-                getArrayBuffer(audioBlob).then((buffer) => {
-                    return new Promise((res, rej) => {
-                        audioContext.decodeAudioData(buffer, res, rej);
+                p.innerHTML = "Recording what you are speaking....";
+                recorder = new MediaRecorder(stream);
+                recorder.start();
+                recorder.ondataavailable = (e) => {
+                    chunks.push(e.data)
+                }
+                // export the stored data into audio clip
+                recorder.onstop = (e) => {
+                    var audioBlob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
+                    var audioContext = new AudioContext();
+                    getArrayBuffer(audioBlob).then((buffer) => {
+                        return new Promise((res, rej) => {
+                            audioContext.decodeAudioData(buffer, res, rej);
+                        });
+                    }).then((buf) => {
+                        p.innerHTML = "playing L's voice";
+                        lvoiceConvertor(audioContext, buf);
                     });
-                }).then((buf) => {
-                    p.innerHTML = "playing L's voice";
-                    lvoiceConvertor(audioContext, buf);
-                });
-            };
+                };
 
-            setTimeout(() => {
-                p.innerHTML = "Stopped the recording.... Playing with L voice";
-                recorder.stop()
-            }, defaultRecordingTimeout);
+                setTimeout(() => {
+                    p.innerHTML = "Stopped the recording.... Playing with L voice";
+                    recorder.stop()
+                }, defaultRecordingTimeout);
 
-        }).catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
 
     } else {
         p.innerHTML = "Browser doesn't support mediarecorder";
